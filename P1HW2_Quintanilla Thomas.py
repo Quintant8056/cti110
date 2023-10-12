b360fcbe93f7e7b1fad1820cd2bd90dfc537579f
Quintant8056 <145589931+Quintant8056@users.noreply.github.com>
2023-10-12 10:49:32 -0400

    Create P1HW2_Quintanilla Thomas.py

---
+++ P1HW2_Quintanilla Thomas.py
@@ -0,0 +1,17 @@
+# A brief description of the project
+
+# Date
+
+# CTI-110 P1HW2 - Travel Expense
+
+# Your Name
+
+#
+
+budget = int(input('Enter Budget: '))
+destination = input('Enter travel destination: ')
+gas_expense = int(input('How much gas will you spend? '))
+accommodation = int(input('How much will you need for accommodation? '))
+food = int(input('How much will you spend on food?'))
+print('-----------Travel Expenses----------')
+print = ('Budget:', budget)
